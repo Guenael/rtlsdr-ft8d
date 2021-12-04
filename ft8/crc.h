b5ad1ea1b395d121eb2ee537058760a715f49090
Guenael <guenael.jouchet@gmail.com>
2021-12-04 02:42:58 -0500

    feat(rtlsdr_ft8d): initial commit

---
+++ ft8/crc.h
@@ -0,0 +1,47 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2018 Kārlis Goba
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+#ifndef _INCLUDE_CRC_H_
+#define _INCLUDE_CRC_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Compute 14-bit CRC for a sequence of given number of bits using FT8/FT4 CRC polynomial
+// [IN] message  - byte sequence (MSB first)
+// [IN] num_bits - number of bits in the sequence
+uint16_t ftx_compute_crc(const uint8_t message[], int num_bits);
+
+/// Extract the FT8/FT4 CRC of a packed message (during decoding)
+/// @param[in] a91 77 bits of payload data + CRC
+/// @return Extracted CRC
+uint16_t ftx_extract_crc(const uint8_t a91[]);
+
+/// Add FT8/FT4 CRC to a packed message (during encoding)
+/// @param[in] payload 77 bits of payload data
+/// @param[out] a91 91 bits of payload data + CRC
+void ftx_add_crc(const uint8_t payload[], uint8_t a91[]);
+
+#endif // _INCLUDE_CRC_H_
---
+++ rtlsdr_ft8d.h
@@ -0,0 +1,119 @@
+/*
+ * FreeBSD License
+ * Copyright (c) 2016, Guenael
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#pragma once
+
+
+#include <unistd.h>
+
+
+#ifndef bool
+	typedef uint32_t bool;
+	#define true  1
+	#define false 0
+#endif
+
+
+struct receiver_state {
+    /* Variables used for stop conditions */
+    bool exit_flag;
+    bool decode_flag;
+
+    /* Buffer used for sampling */
+    float *iSamples;
+    float *qSamples;
+
+    /* Simple index */
+    uint32_t iqIndex;
+};
+
+
+/* Option & config of the receiver */
+struct receiver_options {
+    uint32_t dialfreq;
+    uint32_t realfreq;
+    int32_t  gain;
+    int32_t  autogain;
+    int32_t  ppm;
+    int32_t  shift;
+    int32_t  upconverter;
+    int32_t  directsampling;
+    int32_t  maxloop;
+    int32_t  device;
+    char     date[7];
+    char     uttime[5];
+};
+
+
+/* Option & config of decoder (Shared with the wsprd code) */
+struct decoder_options {
+    uint32_t freq;         // Dial frequency
+    char     rcall[13];    // Callsign of the RX station
+    char     rloc[7];      // Locator of the RX station
+    char     date[7];      // Date & time of the processes samples
+    char     uttime[5];    //  ''
+    uint32_t quickmode;    // Decoder option & tweak
+    uint32_t usehashtable; //  ''
+    uint32_t npasses;      //  ''
+    uint32_t subtraction;  //  ''
+};
+
+
+struct decoder_results {
+    double   freq;
+    float    sync;
+    float    snr;
+    float    dt;
+    float    drift;
+    int32_t  jitter;
+    char     message[23];
+    char     call[13];
+    char     loc[7];
+    char     pwr[3];
+    uint32_t cycles;
+};
+
+
+static void rtlsdr_callback(unsigned char *samples, uint32_t samples_count, void *ctx);
+static void *rtlsdr_rx(void *arg);
+void postSpots(uint32_t n_results);
+static void *decoder(void *arg);
+double atofs(char *s);
+int32_t parse_u64(char* s, uint64_t* const value);
+void initSampleStorage();
+void initDecoder_options();
+void initrx_options();
+void sigint_callback_handler(int signum);
+void usage(void);
+int32_t readfile(float *iSamples, float *qSamples, char *filename);
+int32_t writefile(float *iSamples, float *qSamples, char *filename, uint32_t type, double freq);
+
+// Cleanup todo
+float hann_i(int i, int N);
+//void extract_power(float *idat, float *qdat,waterfall_t* power, int block_size);
+void ft8_subsystem(float *idat, float *qdat, uint32_t npoints, struct decoder_options options, struct decoder_results *decodes, int32_t *n_results);
